8bc87f4603731db48c1d34bba3f7af1a35201535
Robinson <robicamq@amazon.com>
2026-09-09 08:02:15 -0500

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+```javascript
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-website/',
 })
+```
